b8caabc17978d1c82d0bf696d34f2111807b2ec1
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-08-06 10:54:35 -0300

    Navegação de Componentes de Página

--- src/components/Destinos.jsx
+++ src/components/Destinos.jsx
@@ -1,18 +1,29 @@
 // Destinos.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import FormDestino from './FormDestino';
 import Destino from './Destino';
 
-
 const Destinos = () => {
-    return(
-        <div>
-    <FormDestino adicionarDestino={adicionarDestino} />
-    {destinos.map((destino, index) => (
-  <Destino key={index} nome={destino.nome} descricao={destino.descricao} />
-))}
-</div>
-    );
+
+  const [destinos, setDestinos] = useState([
+    { nome: 'Paris', descricao: 'A cidade do amor.' },
+    { nome: 'Nova York', descricao: 'A cidade que nunca dorme.' },
+    { nome: 'Tóquio', descricao: 'A capital do Japão.' }
+  ]);
+
+  const adicionarDestino = (novoDestino) => {
+    setDestinos([...destinos, novoDestino]);
   };
-  
-  export default Destinos;
+
+
+  return (
+    <div>
+      <FormDestino adicionarDestino={adicionarDestino} />
+      {destinos.map((destino, index) => (
+        <Destino key={index} nome={destino.nome} descricao={destino.descricao} />
+      ))}
+    </div>
+  );
+};
+
+export default Destinos;
